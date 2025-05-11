bf9dd9b7698c458946349da2338b47708a5e3c60
arunima123456 <arunimamondal1994@gmail.com>
2025-05-11 21:43:21 +0530

    11th may,2025

---
+++ src/ex_01_java_basics/Lab005_Comments.java
@@ -0,0 +1,17 @@
+package ex_01_java_basics;
+
+public class Lab005_Comments
+{
+    public static void main(String[] args) {
+        // This is comments not executed
+        // This is comments not executed
+        // This is comments not executed
+        // This is comments not executed
+        // This is comments not executed
+        // This is comments not executed
+        // This is comments not executed
+        // This is comments not executed
+        // This is comments not executed
+        System.out.println("hello world");
+    }
+}
---
+++ src/ex_01_java_basics/Lab009_print_vs_println.java
@@ -0,0 +1,11 @@
+package ex_01_java_basics;
+
+public class Lab009_print_vs_println
+{
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("bye");
+        System.out.print("hi");
+        System.out.print("bye");
+    }
+}
