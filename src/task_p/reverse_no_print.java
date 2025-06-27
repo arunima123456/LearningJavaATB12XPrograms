b88ec72608c9b92f905aaf596f0ea92ff1eda7d3
arunima123456 <arunimamondal1994@gmail.com>
2025-06-27 20:30:59 +0530

    else if task

---
+++ src/task_p/reverse_no_print.java
@@ -0,0 +1,12 @@
+package task_p;
+
+public class reverse_no_print
+{
+    public static void main(String[] args) {
+
+        for(int i=10;i>0;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/task_p/sum_100.java
@@ -0,0 +1,14 @@
+package task_p;
+
+public class sum_100
+{
+    public static void main(String[] args) {
+int sum=0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum+=i;
+        }
+        System.out.println("Total sum: "+sum);
+
+    }
+}
